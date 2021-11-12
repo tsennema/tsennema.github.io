c6dfba161d6c070d68425a9171b754c7c38c7d13
tsennema <89606946+tsennema@users.noreply.github.com>
2021-11-12 16:05:52 -0500

    figuring out filters

--- chooser/scripts.js
+++ chooser/scripts.js
@@ -24,8 +24,8 @@ function populateWheelSelect() {
 function loadSpinPage() {
     if (wheelSelect !== null) {
         loadWheel()
-        loadIncludeList()
-        // etc
+        loadFilterList("included", true)
+        loadFilterList("allowMultiple", true)
     }
     else {
         window.location.replace("../")
@@ -52,33 +52,34 @@ function loadWinner(winners) {
     document.getElementById("winnerSpot").appendChild(winList);
 }
 
-function loadIncludeList() {
-    let includeList = [];
+function loadFilterList(filterName, defaultChecked) {
+    let filterList = [];
     // loop through all headings other than name (should only be 2-3)
     for (let i = 2; i < wheels[wheelSelect].length; i++) {
         // loop through items in the selected wheel
         for (item of wheels[wheelSelect][0]) {
             let temp = item[wheels[wheelSelect][i]];
-            if (includeList.indexOf(temp) === -1) {
-                includeList.push(item[wheels[wheelSelect][i]])
+            if (filterList.indexOf(temp) === -1) {
+                filterList.push(item[wheels[wheelSelect][i]])
             }
         }
     }
     // Having collected all unique identifiers beyond name
-    for (heading of includeList) {
+    for (heading of filterList) {
         let newRow = document.createElement("tr");
         let newCell = document.createElement("td");
 
         let listCheck = document.createElement("input");
         listCheck.type = "checkbox";
-        listCheck.checked = true;
-        listCheck.id = heading;
+        listCheck.checked = defaultChecked;
+        listCheck.id = filterName + heading;
+        listCheck.className = filterName;
 
         let listItem = document.createElement("label");
-        listItem.setAttribute("for", heading);
+        listItem.setAttribute("for", filterName + heading);
         listItem.innerHTML = heading;
 
-        document.getElementById("includeBody").appendChild(newRow).appendChild(newCell).append(listCheck, listItem);
+        document.getElementById(filterName + "Body").appendChild(newRow).appendChild(newCell).append(listCheck, listItem);
     }
 }
 
@@ -90,20 +91,39 @@ function randomSpin() {
     let count = document.getElementById("count").value;
     if (Number.isInteger(parseInt(count)) === false || count < 1 || count > 4) { count = 1; }
     else { count = parseInt(count); }
-    let candidates = wheels[wheelSelect][0];
-
-
-
     // this is where any edits to candidates should happen
+    let candidates = filterCandidates("included", wheels[wheelSelect][0])
+    // collect list of included tags
+    // const includedHTML = document.getElementsByClassName("included");
+    // const included = [];
+    // for (let i = 0; i < includedHTML.length; i++) {
+    //     if (includedHTML.item(i).checked === true) {
+    //         included.push(includedHTML[i].id);
+    //     }
+    // }
+    // // filter by included
+    // const filtered1 = [];
+    // for (candidate of candidates) {
+    //     let checkIfIncluded = true;
+    //     for (let i = 2; i < wheels[wheelSelect].length; i++) {
+    //         let heading = wheels[wheelSelect][i];
+    //         if (included.includes(candidate[heading]) === false) {
+    //             checkIfIncluded = false;
+    //         }
+    //     }
+    //     if (checkIfIncluded) {
+    //         filtered1.push(candidate);
+    //     }
+    // }
 
-
-
+    // Reset list to name candidates so we can comment out filters as needed
     let winners = []
     let escape = 0;
     while (winners.length < count) {
         let temp = Math.floor(Math.random() * candidates.length)
         if (winners.indexOf(candidates[temp]) === -1) {
             winners.push(candidates[temp]);
+            winners = filterCandidates("allowMultiple", winners);
         }
         escape += 1;
         if (escape > 50) { break; }
@@ -111,5 +131,42 @@ function randomSpin() {
     loadWinner(winners);
 }
 
+function filterCandidates(filter, candidates) {
 
+    // find all elements with class and convert to array filterHeadings
+    const filterHeadings = []
+    const filterHTML = document.getElementsByClassName(filter);
+    for (let i = 0; i < filterHTML.length; i++) {
+        if (filterHTML.item(i).checked === true) {
+            filterHeadings.push(filterHTML[i].id);
+        }
+    }
+    // check if candidate should be added to filtered list
+    const filtered = []
+    for (candidate of candidates) {
+        let checkIfIncluded = true;
+        // It's easy enough to remove candidates before the random selection
+        if (filter === "included") {
+            for (let i = 2; i < wheels[wheelSelect].length; i++) {
+                let heading = wheels[wheelSelect][i];
+                if (filterHeadings.includes(filter + candidate[heading]) === false) {
+                    checkIfIncluded = false;
+                }
+            }
+        }
+        // I don't want the original order to matter, so this filter should be called during the random selection process
+        if (filter === "allowMultiple") {
+            //TODO check every candidate to see if there's matching tags with the last candidate
+            // if there is a match, don't let the last one be added
+            // if there is no match, let it be added unimpeded 
+
+        }
+
+        if (checkIfIncluded) {
+            filtered.push(candidate);
+        }
+    }
+
+    return filtered;
+}
 
